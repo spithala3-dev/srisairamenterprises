be081b5ceac706de8349f6309f3f2ad5a357625c
Pithala Srihari <spithala3@gmail.com>
2026-04-16 11:09:49 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='ssr-v1';
-const ASSETS=['/'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/')))
-));
